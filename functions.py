293f2e51431753bd4a6dfbd24466c6e3c5673ba1
DESKTOP-H9E8IGT <samah.abbas11@gmail.com>
2021-11-04 21:45:16 +0200

    flask app code

---
+++ functions.py
@@ -0,0 +1,54 @@
+import requests
+import numpy as np
+
+
+## Supported functions are :
+# min
+# max
+# mean
+# median
+# pXXX
+
+def min(img):
+    return np.min(img)
+
+def max(img):
+    return np.max(img)
+
+def mean(img):
+    return np.mean(img)
+
+def median(img):
+    return np.median(img)
+
+def percentile(img,p):
+    return np.percentile(img, p) # Compute the p-th percentile of the data along the specified axis
+
+
+
+######### ERROR handing helper functions #################
+
+# checks if func is supported
+def func_IsSupported(func):
+    funcs =['min','max','mean','median']
+
+    if func in funcs or check_p_func(func):
+        return True
+
+# checks that pXXX function is valid -> (p1 - p100)
+def check_p_func(pFunc):
+    if pFunc.startswith('p') and pFunc[1:].isdigit:
+        print(pFunc)
+        if int(pFunc[1:]) in range(1,101):
+            return True
+    return False
+
+# validate image URL
+def validate_img(url):
+   image_formats = ("image/png", "image/jpeg", "image/jpg")
+   r = requests.head(url)
+   if r.headers["content-type"] in image_formats:
+      return True
+   return False
+
+
---
+++ img_Stats.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request, abort
+import requests
+from PIL import Image,ImageOps
+import functions
+import numpy as np
+import xml.etree.ElementTree as ET
+
+
+
+
+app = Flask(__name__)
+
+# stats route (img statistics) --> supports only GET requests
+@app.route('/stats/<img_file_name>/<func_name>', methods = ['GET'])
+def stats(img_file_name , func_name):
+
+    url= "https://storage.googleapis.com/seetree-demo-open/"+img_file_name
+
+    #check if an image does not exist or the function is not supported
+    if not functions.validate_img(url):
+        abort(404)
+
+    #check if the function is not supported
+    if not functions.func_IsSupported(func_name):
+        abort(404)
+
+
+    img = Image.open(requests.get(url, stream=True).raw) #open image from url
+
+    # convert the image to grayscale with PIL ImageOps.grayscale \
+    gray_img = ImageOps.grayscale(img)
+
+    # then pass grayscale image it to np.asarray(),
+    # it returns 2D ndarray whose shape is (row (height), column (width)).
+    arr_img = np.asarray(gray_img)
+
+    # handle calculations --> call methods in functions class
+    if not functions.check_p_func(func_name):
+        method_to_call = getattr(functions, func_name)
+        result = method_to_call(arr_img)
+    else : # in case that the entered func is pXXX
+        method_to_call  = getattr(functions, "percentile")
+        result = method_to_call(arr_img,int(func_name[1:]))
+
+
+
+    return render_template('stats.html',img_file_name=img_file_name,func_name=func_name,res=result)
+
+
+# health route --> supports only GET requests
+@app.route('/health', methods = ['GET'])
+def health():
+    return render_template('health.html')
+
+# root route (the main page) --> supports only GET requests
+@app.route('/', methods = ['GET'])
+def root_route():
+    return render_template('root.html')
+
+# In case of choosing images from gallery page and choosing function
+@app.route('/stats', methods = ['GET'])
+def images():
+    return render_template('images.html')
+
+@app.route("/stats/<img_file_name>")                          # after they choose an image they can choose the function 
+def choose_Func(img_file_name):
+    return render_template("choose_Func.html",img_file_name=img_file_name)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+   return render_template('404.html', title = '404'), 404
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8181)
